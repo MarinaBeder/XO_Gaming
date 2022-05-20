c63d325fe0fb9a0af8f40ac448fc6093cd7847dd
sara823 <66432441+sara823@users.noreply.github.com>
2022-05-20 18:22:50 +0200

    Update Port.c

--- Port.c
+++ Port.c
@@ -39,3 +39,9 @@ void TurnX(void)
     GPIO_PORTF_DATA_R |=(1<<1);
     GPIO_PORTF_DATA_R &= ~(1<<2);
 }
+void TurnO(void)
+{
+    GPIO_PORTF_DATA_R &= ~(1<<1);
+    GPIO_PORTF_DATA_R |=(1<<2);
+}
+
